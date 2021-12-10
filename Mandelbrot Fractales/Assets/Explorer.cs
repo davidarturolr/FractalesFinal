ea2819cfecfd1ccfa691fab7dd10f1b957a0a2c9
David Leon <da.leon@uniandes.edu.co>
2021-12-10 16:09:13 -0500

    Fractales Final

---
+++ Mandelbrot Fractales/Assets/Explorer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explorer : MonoBehaviour
+{
+    public Material mat;
+    public Vector2 pos;
+    public float scale, angle;
+
+    private Vector2 smoothPos;
+    private float smoothScale, smoothAngle;
+
+
+    private void UpdateShader()
+    {
+        smoothPos = Vector2.Lerp(smoothPos, pos, .03f); // cada vez acerca al vector smoothpos al vector pos en un factor .03 , y cuando se mueva el vector posicion para que no hayan paradas abruptas
+        smoothScale = Mathf.Lerp(smoothScale, scale, .03f); // para que no hayan paradas abruptas el vector scale se re reemplaza con smooth scale 
+        smoothAngle = Mathf.Lerp(smoothAngle, angle, .03f);
+
+        float aspect = (float)Screen.width / (float)Screen.height; // arregla el aspect ratio cuando se da play a Game (mi pc es 16:9 otros 4:3) asi el fractal no se deforma
+
+        float scaleX = smoothScale;
+        float scaleY = smoothScale;
+
+        if (aspect > 1f)
+            scaleY /= aspect;
+        else
+            scaleX *= aspect;
+
+        mat.SetVector("_Area", new Vector4(smoothPos.x, smoothPos.y, scaleX, scaleY));
+        mat.SetFloat("_Angle", smoothAngle);
+    }
+
+    private void HandleInputs()
+    {
+        if (Input.GetKey(KeyCode.M)) // M para que haga zoom
+        scale *= .99f; // se reduce la escala a nivel de porcentaje, porque si es un valor fijo se vería como si cada vez el zoom fuera más rapido.
+        if (Input.GetKey(KeyCode.Z)) // z para que haga zoom out
+            scale *= 1.01f; // se maneja la escala a nivel de porcentaje, porque si es un valor fijo se vería como si cada vez el zoom fuera más rapido.
+
+        Vector2 dir = new Vector2(.01f * scale, 0);
+        float s = Mathf.Sin(angle);
+        float c = Mathf.Cos(angle);
+        dir = new Vector2(dir.x*c, dir.x*s); // para que cuando se gire la derecha siga siendo la derecha
+        if (Input.GetKey(KeyCode.A))
+            pos -= dir;
+        if (Input.GetKey(KeyCode.D))
+            pos += dir;
+
+        dir = new Vector2(-dir.y, dir.x);// ajusta para y 
+        if (Input.GetKey(KeyCode.S))
+            pos -= dir;
+        if (Input.GetKey(KeyCode.W)) // para moverse
+            pos += dir;
+
+        if (Input.GetKey(KeyCode.E)) // para que rote
+            angle -= .01f; 
+        if (Input.GetKey(KeyCode.Q)) 
+            angle += .01f;
+
+
+    }
+
+    // cambio a Fixed para que se ejectute ciertas veces por segundo, no de acuerdo a la velocidad del pc 
+    void FixedUpdate() 
+    {
+        HandleInputs();
+        UpdateShader();
+    }
+}
---
+++ Mandelbrot Fractales/Assets/Mandelbrot.shader
@@ -0,0 +1,76 @@
+﻿Shader "Explorer/Mandelbrot"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Area("Area", vector) = (0,0,4,4)
+        _Angle ("Angle", range(-3.1415,3.1415))=0
+        _MaxIter("Iterations", range(4,1000))=255
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+            float4 _Area;
+            float _Angle, _MaxIter;
+            sampler2D _MainTex;
+
+
+            float2 rot(float2 p, float2 pivot, float a)
+            {   float s = sin(a);
+                float c = cos(a);
+                p -= pivot; //para que el pivote este en 0,0
+                p = float2(p.x*c-p.y*s,p.x*s+p.y*c); // lo rotamos
+                p += pivot; 
+
+                   return p;
+            }
+
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float2 c = _Area.xy + (i.uv -.5)*_Area.zw; //las coordenadas en los vectrores van xyzw por eso la w
+                c = rot(c, _Area.xy, _Angle); //rotacion
+                float2 z; // para saber donde está saltando el pixel en la pantalla
+                float iter;
+                for (iter = 0; iter < _MaxIter; iter ++){ //controla el numero de iteraciones
+                z = float2(z.x*z.x-z.y*z.y, 2*z.x*z.y) + c ; // actualiza el valor c, basado en el valor pasado de c.                
+                if (length(z)> 2) break; // la distancia del vector al origen 
+
+            }
+            float m = sqrt(iter/_MaxIter);
+            float4 col = sin(float4(.3,.45,.61,1)*m*20)*.5+.5;
+            return col;
+            }
+            ENDCG
+        }
+    }
+}
